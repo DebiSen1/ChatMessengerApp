d55df7f6aa437a942ed9641568597f1433c4535d
DebiSen1 <debi.sen@beas.co.in>
2022-09-27 12:24:02 +0530

    initial setup

---
+++ src/main/java/com/debisen/ChatMessengerApp/ChatMessengerAppApplication.java
@@ -0,0 +1,13 @@
+package com.debisen.ChatMessengerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMessengerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatMessengerAppApplication.class, args);
+	}
+
+}
